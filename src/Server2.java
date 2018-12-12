2b61e8269c8fb595beae32d91054af28abef81e2
김산하 <37866021+Nachasan@users.noreply.github.com>
2018-12-13 01:25:29 +0900

    Update Server2.java

--- src/Server2.java
+++ src/Server2.java
@@ -4,6 +4,7 @@ import java.util.*;
 
 public class Server2 {
 	private ServerSocket serverSocket;
+	private Manager Man = new Manager();
 
 	public Server2() {
 	}
@@ -14,10 +15,10 @@ public class Server2 {
 			System.out.println("辑滚家南 积己");
 			while (true) {
 				Socket socket = serverSocket.accept();
-				
-				gameThread gt= new gameThread(socket);
+
+				gameThread gt = new gameThread(socket);
 				gt.start();
-			
+
 			}
 		} catch (Exception e) {
 		}
@@ -27,26 +28,143 @@ public class Server2 {
 		Server2 s2 = new Server2();
 		s2.StartServer();
 	}
-}
 
-class gameThread extends Thread {
-	Socket socket;
-	Boolean isThread;
-	gameThread(Socket socket){
-		this.socket = socket;
-	}
-	
-	public void run() {
-		try {
-			ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
-			ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
-			
-			String str;
-			
-			while(isThread) {
-				
+	class gameThread extends Thread {
+		private Socket socket;
+		private int roomNumber = -1;
+		private String userName = null;
+		private boolean ready = false;
+
+		private BufferedReader reader;
+		private PrintWriter writer;
+
+		gameThread(Socket socket) {
+			this.socket = socket;
+		}
+
+		Socket getSocket() {
+			return socket;
+		}
+
+		int getRoomNumber() {
+			return roomNumber;
+		}
+
+		String getUserName() {
+			return userName;
+		}
+
+		boolean isReady() {
+			return ready;
+		}
+
+		public void run() {
+			try {
+				reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				writer = new PrintWriter(socket.getOutputStream(), true);
+
+				ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+				ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+
+				String str;
+
+				while ((str = reader.readLine()) != null) {
+					if (str.startsWith("[NAME]"))
+						userName = str.substring(6);
+
+					else if (str.startsWith("[ROOM]")) {
+						int roomNum = Integer.parseInt(str.substring(6));
+						if (Man.isFull(roomNum)) {
+							if (roomNumber != -1)
+								Man.sendToOthers(this, "[EXIT]" + userName);
+							roomNumber = roomNum;
+							writer.println(str);
+							writer.println(Man.getNameInRoom(roomNumber));
+							Man.sendToOthers(this, "[ENTER]" + userName);
+						} else
+							writer.println("[FULL}");
+					}
+				}
+			} catch (Exception e) {
 			}
-		} catch (Exception e) {
+		}
+	}
+
+	class Manager extends Vector {
+		Manager() {
+		}
+
+		void add(gameThread gt) {
+			super.add(gt);
+		}
+
+		void remove(gameThread gt) {
+			super.remove(gt);
+		}
+
+		gameThread getGT(int i) {
+			return (gameThread) elementAt(i);
+		}
+
+		Socket getSocket(int i) {
+			return getGT(i).getSocket();
+		}
+
+		void sendTo(int i, String str) {
+			try {
+				PrintWriter pw = new PrintWriter(getSocket(i).getOutputStream(), true);
+				pw.println(str);
+			} catch (Exception e) {
+			}
+		}
+
+		int getRoomNumber(int i) {
+			return getGT(i).getRoomNumber();
+		}
+
+		synchronized boolean isFull(int roomNum) {
+			if (roomNum == 0)
+				return false;
+			int count = 0;
+			for (int i = 0; i < size(); i++) {
+				if (roomNum == getRoomNumber(i))
+					count++;
+			}
+			if (count >= 2)
+				return true;
+			return false;
+		}
+
+		void sendToRoom(int roomNum, String str) {
+			for (int i = 0; i < size(); i++) {
+				if (roomNum == getRoomNumber(i))
+					sendTo(i, str);
+			}
+		}
+
+		void sendToOthers(gameThread gt, String str) {
+			for (int i = 0; i < size(); i++) {
+				if (getRoomNumber(i) == gt.getRoomNumber() && getGT(i) != gt)
+					sendTo(i, str);
+			}
+		}
+
+		synchronized boolean isReady(int roomNum) {
+			int count = 0;
+			for (int i = 0; i < size(); i++)
+				if (roomNum == getRoomNumber(i) && getGT(i).isReady())
+					count++;
+			if (count == 2)
+				return true;
+			return false;
+		}
+
+		String getNameInRoom(int roomNum) {
+			StringBuffer sb = new StringBuffer("[PLAYERS]");
+			for (int i = 0; i < size(); i++)
+				if (roomNum == getRoomNumber(i))
+					sb.append(getGT(i).getUserName() + "\t");
+			return sb.toString();
 		}
 	}
 }
